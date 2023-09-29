4a51696b0207bb977e115583b12519ddfa10186a
nazlinurakyol <nazlinurakyol@gmail.com>
2023-09-29 02:20:35 +0200

    change icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import HighlightIcon from '@mui/icons-material/Highlight';
 import SouthIcon from '@mui/icons-material/South';
-import PetsIcon from '@mui/icons-material/Pets';
+import { GiMouthWatering } from 'react-icons/gi';
+
 
 function Header() {
   return <header>
-    <h1> <PetsIcon /> Snapper</h1>
+    <h1> <GiMouthWatering /> Snapper</h1>
     <h2 style={{ textAlign: 'center', verticalAlign: 'middle' }}> Snap here <SouthIcon /></h2> 
   </header>;
 }
