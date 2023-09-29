c277c3d13917aa5171303a6ae37884ce2235091b
nazlinurakyol <nazlinurakyol@gmail.com>
2023-09-29 02:03:07 +0200

    change icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import HighlightIcon from '@mui/icons-material/Highlight';
 import SouthIcon from '@mui/icons-material/South';
+import PetsIcon from '@mui/icons-material/Pets';
 
 function Header() {
   return <header>
-    <h1> <HighlightIcon /> Snapper</h1>
+    <h1> <PetsIcon /> Snapper</h1>
     <h2 style={{ textAlign: 'center', verticalAlign: 'middle' }}> Snap here <SouthIcon /></h2> 
   </header>;
 }
