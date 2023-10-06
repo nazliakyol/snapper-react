7c6dbf3b446f4a32e3658777b4e86d2b3e46a5fd
nazlinurakyol <nazlinurakyol@gmail.com>
2023-10-06 17:44:24 +0200

    initial commit

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,7 +5,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 function Note(props) {
 
   return (
-    <div className="note">
+    <div className="note">  
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button
